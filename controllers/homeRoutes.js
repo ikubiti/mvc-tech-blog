12dcef0557fcd25afb1f3ad5c7df647fd99c92d0
ikubiti <ikubiti@icloud.com>
2023-02-23 13:38:55 -0500

    Added definition for the homepage route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -9,14 +9,13 @@ router.get('/', async (req, res) => {
       include: [
         {
           model: Users,
-          attributes: ['name', 'username'],
+          attributes: ['name', 'email'],
         },
       ],
     });
 
     // Serialize data so the template can read it
     const allPosts = allPostedBlogs.map((post) => post.get({ plain: true }));
-
     // Pass serialized data and session flag into template
     res.render('homepage', {
       allPosts,
@@ -27,10 +26,54 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/posts/:id', async (req, res) => {
+  try {
+    const blogData = await Posts.findByPk(req.params.id, {
+      include: [
+        {
+          model: Users,
+          attributes: ['name', 'email', 'username'],
+        },
+      ],
+    });
+
+    const aPost = blogData.get({ plain: true });
+    console.log(aPost);
+
+    res.render('posts', {
+      ...aPost,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Use withAuth middleware to prevent access to route
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await Users.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Posts }, { model: Comments }],
+    });
+
+    const user = userData.get({ plain: true });
+    console.log(user);
+
+    res.render('dashboard', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect('/');
     return;
   }
 
